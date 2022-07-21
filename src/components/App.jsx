a42f8cb3d29b78d4d7c3e61b942ec2f0f884a579
Kyrylo1122 <93613807+Kyrylo1122@users.noreply.github.com>
2022-07-21 14:24:36 +0200

    add all phonebook files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,53 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/Contactform';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { Box } from './Box/Box';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+  onSubmitForm = data => {
+    const isExist = this.isExistedName(data.name);
+    if (isExist) {
+      alert('Name already exist');
+      return;
+    }
+    const newCon = this.state.contacts;
+    newCon.push(data);
+    this.setState({ contacts: newCon });
+    // this.setState(p => p.contacts.push(data));
+  };
+  isExistedName = name => this.state.contacts.map(e => e.name).includes(name);
+
+  deleteContact = id => {
+    this.setState(p => ({ contacts: p.contacts.filter(e => e.id !== id) }));
+  };
+  render() {
+    const { filter, contacts } = this.state;
+    const visibleContacts = contacts.filter(e =>
+      e.name.toLowerCase().includes(filter.toLowerCase())
+    );
+    return (
+      <Box display="flex">
+        <Box ml="auto" mr="auto" textAlign="center">
+          <h1>Phonebook</h1>
+          <ContactForm onSubmitForm={this.onSubmitForm} />
+          <h2>Contacts</h2>
+          <Filter changeFilter={this.changeFilter} />
+          <ContactList
+            contacts={visibleContacts}
+            deleteCon={this.deleteContact}
+          />
+        </Box>
+      </Box>
+    );
+  }
+}
+export default App;
